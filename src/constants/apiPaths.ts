462f112fe1f8739eb4f35d33ad457919de5bcffb
Anton Paskanny <anton.paskanny@gmail.com>
2024-07-27 21:58:19 +0200

    feat: add cart endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://xx9hwhlzx2.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://46nyda44ee.execute-api.eu-west-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
