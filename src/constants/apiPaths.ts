76c5b193933e014051ca86eba99187fd47bd8d4a
Anton Paskanny <anton.paskanny@gmail.com>
2024-08-05 16:25:49 +0200

    feat: add proxy url for cart and order endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://gyc0omak64.execute-api.eu-west-1.amazonaws.com/prod",
-  order: "https://46nyda44ee.execute-api.eu-west-1.amazonaws.com/prod",
+  order: "https://7b9uwo8de5.execute-api.eu-west-1.amazonaws.com/prod",
   import: "https://xx9hwhlzx2.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://46nyda44ee.execute-api.eu-west-1.amazonaws.com/prod",
+  cart: "https://7b9uwo8de5.execute-api.eu-west-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
